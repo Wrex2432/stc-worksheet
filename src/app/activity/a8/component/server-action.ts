6d896d751b94c186dc50cb4c6705fbc804e38c74
Wrex2432 <wrex2432@gmail.com>
2025-01-25 16:54:57 +0800

    __

--- src/app/activity/a8/component/server-action.ts
+++ src/app/activity/a8/component/server-action.ts
@@ -5,18 +5,22 @@ import { supabase } from "@/app/utils/supabaseClient";
 export async function addTask(formData: FormData): Promise<void> {
     const task = formData.get("name");
     const {error} = await supabase.from('tasks').insert([{task}]).single();
+    if (error) console.error(error);
     revalidatePath("/activity/a8");
 }
 export async function updateTask(updatedTask: string, id: string | number): Promise<void> {
     const {error} = await supabase.from('tasks').update({task:updatedTask}).eq('id', id);
-    // revalidatePath("/activity/a8");
+    if (error) console.error(error);
+    revalidatePath("/activity/a8");
 }
 export async function updateStatus(updatedStatus: boolean, id: string | number): Promise<void> {
     const {error} = await supabase.from('tasks').update({isDone:updatedStatus}).eq('id', id);
-    // revalidatePath("/activity/a8");
+    if (error) console.error(error);
+    revalidatePath("/activity/a8");
 }
 
 export async function removeTask(id: string | number): Promise<void> {
     const {error} = await supabase.from('tasks').delete().eq('id',id);
+    if (error) console.error(error);
     revalidatePath("/activity/a8");
 }
